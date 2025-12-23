4e980e873de409319aaaadfed52914789c09c819
Grigory Poleshchuk <hitrulein@gmail.com>
2025-12-23 15:41:18 +0300

    build: добавит svelte kit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>SvelteKit App</title>
+</svelte:head>
+
+{@render children()}
