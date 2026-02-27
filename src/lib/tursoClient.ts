ae17b6a7553d1d6397d6ef0ad937385406eda08b
Grigory Poleshchuk <hitrulein@gmail.com>
2026-02-27 18:45:18 +0300

    fix: env issues

--- src/lib/tursoClient.ts
+++ src/lib/tursoClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@libsql/client';
-import { TURSO_DATABASE_URL, TURSO_AUTH_TOKEN } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 export const turso = createClient({
-  url: TURSO_DATABASE_URL,
-  authToken: TURSO_AUTH_TOKEN,
+  url: env.TURSO_DATABASE_URL,
+  authToken: env.TURSO_AUTH_TOKEN,
 });
